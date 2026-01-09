27e2d8d5c2ee04516143ad0a6b32481729eb055b
Jia Yu <jiayu@apache.org>
2026-01-08 17:52:54 -0700

    Release Apache Sedona 1.8.1 (#15)
    * 1.8.1 python files
    
    * use uv instead

--- python/tests/sql/test_function.py
+++ python/tests/sql/test_function.py
@@ -2493,3 +2493,50 @@ class TestPredicateJoin(TestBase):
             pt_row[0] for pt_row in point_df.selectExpr("ST_CoordDim(geom)").collect()
         ]
         assert point_row == [3]
+
+    def test_st_straight_skeleton(self):
+        polygon_wkt = "POLYGON ((1 1, 1 3, 4 3, 4 1, 1 1))"
+        actual_df = self.spark.sql(
+            f"SELECT ST_StraightSkeleton(ST_GeomFromText('{polygon_wkt}')) AS geom"
+        )
+        actual = actual_df.take(1)[0][0]
+        assert actual is not None
+        assert actual.geom_type == "MultiLineString"
+
+        # Test with maxVertices parameter
+        complex_polygon_wkt = "POLYGON ((0 0, 4 0, 4 4, 0 4, 0 0))"
+        actual_df_with_max = self.spark.sql(
+            f"SELECT ST_StraightSkeleton(ST_GeomFromText('{complex_polygon_wkt}'), 10) AS geom"
+        )
+        actual_with_max = actual_df_with_max.take(1)[0][0]
+        assert actual_with_max is not None
+        assert actual_with_max.geom_type == "MultiLineString"
+
+    def test_st_approximate_medial_axis(self):
+        polygon_wkt = "POLYGON ((1 1, 1 3, 4 3, 4 1, 1 1))"
+        actual_df = self.spark.sql(
+            f"SELECT ST_ApproximateMedialAxis(ST_GeomFromText('{polygon_wkt}')) AS geom"
+        )
+        actual = actual_df.take(1)[0][0]
+        assert actual is not None
+        assert actual.geom_type == "MultiLineString"
+
+        # Test with maxVertices parameter
+        complex_polygon_wkt = "POLYGON ((0 0, 4 0, 4 4, 0 4, 0 0))"
+        actual_df_with_max = self.spark.sql(
+            f"SELECT ST_ApproximateMedialAxis(ST_GeomFromText('{complex_polygon_wkt}'), 10) AS geom"
+        )
+        actual_with_max = actual_df_with_max.take(1)[0][0]
+        assert actual_with_max is not None
+        assert actual_with_max.geom_type == "MultiLineString"
+
+    def test_st_oriented_envelope(self):
+        actual = self.spark.sql(
+            "SELECT ST_AsText(ST_OrientedEnvelope(ST_GeomFromText('POLYGON ((0 0, 1 0, 5 4, 4 4, 0 0))')))"
+        ).take(1)[0][0]
+        assert actual == "POLYGON ((0 0, 4.5 4.5, 5 4, 0.5 -0.5, 0 0))"
+
+        actual = self.spark.sql(
+            "SELECT ST_AsText(ST_OrientedEnvelope(ST_GeomFromText('POINT (1 2)')))"
+        ).take(1)[0][0]
+        assert actual == "POINT (1 2)"
